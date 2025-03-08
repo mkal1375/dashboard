1bfe167577891b572284469db27da8c966cac461
Mahdi Kalhor <mkal1375@gmail.com>
2025-03-08 12:17:29 +0330

    feat: add manifest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Contactly',
+    short_name: 'Contactly',
+    description: 'Just another demo application',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#0c53c1',
+    theme_color: '',
+    icons: [
+      {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+}
